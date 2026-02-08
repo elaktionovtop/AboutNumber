14e49aa134c4248e68726e6a48c12688074fe608
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-02-08 09:16:49 +0300

    Возможность повторения

--- Program.cs
+++ Program.cs
@@ -6,35 +6,48 @@ using static System.Console;
 
 WriteTitle("Свойства числа");
 
-Write("Введите число в интервале [-999;999]: ");
-bool result = int.TryParse(ReadLine(), out int number);
-while (!result || number < -999 || number > 999)
+bool repeat;
+do
 {
-    Write("Некорректный ввод. Введите число в интервале [-999;999]: ");
-    result = int.TryParse(ReadLine(), out number);
-}
-
-if (number < 0)
-    Write("Отрицательное ");
-else if (number > 0)
-    Write("Положительное ");
-else
-    Write("Нулевое ");
-                      
-if (number % 2 == 0)
-    Write("Четное ");
-else
-    Write("Нечетное ");
-
-if (number >= -9 && number <= 9)
-    Write("Однозначное");
-else if (number >= -99 && number <= 99)
-    Write("Двузначное");
-else
-    Write("Трехзначное");
+    DescribeNumber();
+    WriteLine();
+    Write("Хотите продолжить? (д/н): ");
+    string answer = ReadLine();
+    repeat = answer.Trim().ToLower() == "д";
+    WriteLine();
+} while (repeat);
 
 ExitApp();
 
+void DescribeNumber()
+{
+    Write("Введите число в интервале [-999;999]: ");
+    bool result = int.TryParse(ReadLine(), out int number);
+    while(!result || number < -999 || number > 999)
+    {
+        Write("Некорректный ввод. Введите число в интервале [-999;999]: ");
+        result = int.TryParse(ReadLine(), out number);
+    }
+
+    if(number < 0)
+        Write("Отрицательное ");
+    else if(number > 0)
+        Write("Положительное ");
+    else
+        Write("Нулевое ");
+
+    if(number % 2 == 0)
+        Write("Четное ");
+    else
+        Write("Нечетное ");
+
+    if(number >= -9 && number <= 9)
+        Write("Однозначное");
+    else if(number >= -99 && number <= 99)
+        Write("Двузначное");
+    else
+        Write("Трехзначное");
+}
 
 void WriteTitle(string title)
 {
