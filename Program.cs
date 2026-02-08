0e975597be208e404e2be7ff58a9eb7dd77bd639
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-02-08 08:54:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Пользователь вводит число в интервал [-999;999].
+Вывести описание этого числа: положительное/отрицательное, четное/нечетное, сколько знаков.
+*/
+
+WriteTitle("Свойства числа");
+
+ExitApp();
+
+
+void WriteTitle(string title)
+{
+    Console.WriteLine(title);
+    Console.WriteLine(new string('-', title.Length));
+    Console.WriteLine();
+}
+
+void ExitApp()
+{
+    Console.WriteLine();
+    Console.Write("Для выхода нажми Enter");
+    Console.ReadLine();
+}
